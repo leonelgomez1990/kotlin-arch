1dc71e7acfbc531e1e38c87721fd2e43c4bc66a2
leonelgomez1990 <leonelgomez@live.com.ar>
2022-02-07 23:40:28 -0300

    hilt annotations

--- Hilt/app/src/main/java/com/leo/hilt/ui/MainActivity.kt
+++ Hilt/app/src/main/java/com/leo/hilt/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.leo.hilt.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.leo.hilt.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- Hilt/app/src/main/java/com/leo/hilt/ui/fragments/ListFragment.kt
+++ Hilt/app/src/main/java/com/leo/hilt/ui/fragments/ListFragment.kt
@@ -1,33 +1,32 @@
 package com.leo.hilt.ui.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.leo.hilt.R
+import androidx.fragment.app.viewModels
+import com.leo.hilt.databinding.ListFragmentBinding
 import com.leo.hilt.ui.viewmodel.ListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ListFragment : Fragment() {
 
     companion object {
         fun newInstance() = ListFragment()
     }
 
-    private lateinit var viewModel: ListViewModel
+    private val viewModel: ListViewModel by viewModels()
+    private lateinit var binding: ListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.list_fragment, container, false)
+        binding = ListFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
